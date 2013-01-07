0c1893126001afed744e7bf9bc5f15ba4f785c52
Will Claiborne <wclaiborne@ign.com>
2013-01-06 18:33:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParticipationPoints::Application.config.secret_token = '91371758d88a5d183ae589b3715025b77fea3e15e5036398c735a2a64d52da126adc69e02b89a43613abe229431991d4251f4ac81d3a3efc89ee8e4e961021a2'
