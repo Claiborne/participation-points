caec62c8dd5dad45c18d7367c2e0339162a187f4
Will Claiborne <wclaiborne@iMac.local>
2013-05-25 20:53:24 -0700

    remove syntax error that 1.9.2 ignores but 1.9.3 doesnt

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,6 @@ class Student < ActiveRecord::Base
   attr_accessible :name, :period, :answered, :not_answered, :teams, :not_teams, :volunteered
 
   validates :name, :presence => true
-  validates :period, :presence => true,
+  validates :period, :presence => true
 
 end
